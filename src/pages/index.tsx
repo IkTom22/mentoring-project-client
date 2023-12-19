3f4df8c1cc4948f6c2cb2bc303d7624e172c1149
Iko <ikotominaga@gmail.com>
2023-12-20 11:12:06 +1300

    we added auth condition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,26 +5,23 @@ import { useEffect, useState } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [email, setEmail] =useState("")
-  const [password, setPassword] = useState("")
   useEffect(() =>{
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/services`).then((response) =>response.json()).then(data =>console.log(data))
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/services`, {credentials: 'include',})
+    .then((response) =>{
+      if(response.ok){
+        return response.json()
+      }else {
+        throw new Error("Unathorized")
+      }
+    })
+    .then(data =>console.log(data))
+    .catch(err =>console.error(err))
   },[])
-  function handleSubmit (e){
-    // console.log(e)
-    e.preventDefault()
-    console.log(`Email: ${email}, Password: ${password}`)
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/sign_in?email=${email}&password=${password}`, {method: 'POST', credentials: 'include',}).then((response) =>response.json()).then(data =>console.log(data))
-    
-  }
+ 
   return (
     <main>
       <h1>The Impact Directory</h1>
-      <form className='flex flex-col max-w-4xl gap-2'>
-        <input className='border-2' type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        <input className='border-2' type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-        <button onClick={handleSubmit}>Login</button>
-      </form>
+      
     </main>
   )
 }
